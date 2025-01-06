f34147994679db4399e3d027b46d69c398b4ded9
Akobir <akobirabduvahobov@gmail.com>
2025-01-06 17:02:59 +0500

    2.11

---
+++ 2.11dars/2.11dars.Api/Program.cs
@@ -0,0 +1,148 @@
+﻿using System.Numerics;
+
+namespace _2._11dars.Api;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+
+        //Dictionary<int, string> map = new Dictionary<int, string>();
+
+        //map.Add(3, "salom");
+
+        //map.TryGetValue(3, out var name);
+
+        //SortedList<int, string> keyValuePairs = new SortedList<int, string>();
+
+        //keyValuePairs.Add(9, "hello");
+        //keyValuePairs.Add(3, "ok");
+        //keyValuePairs.Add(4, "salom");
+        //keyValuePairs.Add(5, "privet");
+        //keyValuePairs.Add(6, "konicheva");
+
+        //var res = keyValuePairs.First();
+
+        //foreach (var pair in keyValuePairs)
+        //{
+        //    Console.WriteLine(pair);
+        //}
+
+
+        //Console.WriteLine(res);
+
+
+
+        //LinkedList<int> list = new LinkedList<int>();
+        //list.AddLast(3);
+        //list.AddLast(4);
+
+        //list.AddFirst(5);
+
+        //var linkendListNode = list.Find(9);
+        
+        //list.AddAfter(linkendListNode, 8);
+
+        //foreach (int i in list)
+        //{
+        //    Console.WriteLine(i);
+        //}
+
+
+        //SortedSet<int> ints = new SortedSet<int>();
+
+        //ints.Add(5);
+        //ints.Add(5);
+        //ints.Add(2);
+        //ints.Add(9);
+        //ints.Add(4);
+        //ints.Add(1);
+        //ints.Add(15);
+
+        //foreach (int i in ints)
+        //{
+        //    Console.WriteLine(i);
+        //}
+
+        //SortedSet<string> strings = new SortedSet<string>();
+
+        //strings.Add("salom");
+        //strings.Add("alik");
+        //strings.Add("botir");
+        //strings.Add("qahramon");
+        //strings.Add("al");
+
+        //foreach (string s in strings)
+        //{
+        //    Console.WriteLine(s);
+        //}
+
+
+
+        //HashSet<int> set = new HashSet<int>();
+
+        //set.Add(1);
+        //Console.WriteLine(set.EnsureCapacity(0));
+        //set.Add(2);
+        //Console.WriteLine(set.EnsureCapacity(0));
+        //set.Add(3);
+        //Console.WriteLine(set.EnsureCapacity(0));
+        //set.Add(4);
+        //Console.WriteLine(set.EnsureCapacity(0));
+        //set.Add(5);
+        //Console.WriteLine(set.EnsureCapacity(0));
+        //set.Add(6);
+        
+
+        
+
+
+     
+
+
+
+        //Console.WriteLine(set.Count);
+
+        //foreach (int i in set)
+        //{
+        //    Console.WriteLine(i);
+        //}
+
+
+
+
+
+        //int? num = 7;
+        //int res;
+        //if(num.HasValue)
+        //{
+        //    res = num.Value;
+        //}
+        //else
+        //{
+        //    res = 0;
+        //}
+
+        //int? num1 = 15;
+
+        //int res = num1 ?? 20;
+        //int res1 = num1 == null ? 0 : num1.Value;
+
+        //Console.WriteLine(res);
+        //Console.WriteLine(res1);
+
+        //int take = 15;
+
+        //take = take <= 0 || take > 100 ? 100 : take;    
+
+
+        //int? intValue = 0;
+
+        //int res = intValue ??= 10;
+
+        //Console.WriteLine(intValue);
+        //Console.WriteLine(res);
+
+
+    }
+}
--- test/test/Program.cs
+++ test/test/Program.cs
@@ -1,40 +1,21 @@
-﻿using System.Collections;
-using test;
-
-class Program
+﻿class Program
 {
     static void Main(string[] args)
     {
-        int a1 = 45;
-        string s1 = "abc";
-        Person person = new Person();
+        var hashSet = new HashSet<int> { 1, 2, 8 };
+        hashSet.Add(1);
+        hashSet.Add(4);
 
-        object obj1 = a1;
-        object obj2 = s1;
-        object obj3 = person;
-
-        ArrayList list = new ArrayList();
-
-        Dictionary<string, string> dic = new Dictionary<string, string>();
-
-        LinkedList<string> lst = new LinkedList<string>();  
-
-        Stack stack = new Stack();
-        stack.Push("a");
-        stack.Push(3);
-        Stack<string> stackStrings = new Stack<string>();
-        stackStrings.Push("a");
-        stackStrings.Push("b");
-        stackStrings.Push("c");
-        stackStrings.Push("d");
-
-        Console.WriteLine(stackStrings.Peek());
-        Console.WriteLine(stackStrings.Peek());
-
-        Console.WriteLine(stackStrings.Pop());
-        Console.WriteLine(stackStrings.Peek());
-
-
+        foreach (var hash in hashSet)
+        {
+            Console.WriteLine(hash);
+        }
 
     }
+}
+
+public class Person
+{
+    public string FirstName { get; set; }
+    public int Age { get; set; }
 }
